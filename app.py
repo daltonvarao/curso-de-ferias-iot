4b70b44a64b13da3dc30af01347d014a4fe7bdcd
daltonvarao <daltonphellipe@gmail.com>
2020-01-18 13:46:52 -0300

    add server file

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+from mqtt import mqtt
+from socket_io import socketio
+import service
+
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'ih6cep2-s1dck4n-in89ons-d2dd3fn-ws3dj0s'
+app.config['DEBUG'] = True
+
+
+mqtt.init_app(app)
+socketio.init_app(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+socketio.run(app)
