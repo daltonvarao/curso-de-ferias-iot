300dd12273c958173fb7c4031e333cc93c61bd3b
daltonvarao <daltonphellipe@gmail.com>
2020-01-18 13:47:47 -0300

    add service handler file

---
+++ service.py
@@ -0,0 +1,23 @@
+from mqtt import mqtt
+from socket_io import socketio
+
+
+status_led = 'off'
+
+@socketio.on('connect')
+def on_connect():
+    global status_led
+    socketio.emit('status-led', status_led)
+
+
+@socketio.on('action')
+def on_message(message):
+    global status_led
+    mqtt.publish('action-led', status_led)
+
+
+@mqtt.on_message()
+def on_status_message(client, userdata, message):
+    global status_led
+    status_led = message.payload.decode()
+    socketio.emit('status-led', status_led)
